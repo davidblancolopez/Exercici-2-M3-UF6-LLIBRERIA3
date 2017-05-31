7fc0e2f3188bee99ea1b48db0976402b48f6af3c
davidblancolopez <daviddbl94@gmail.com>
2017-05-31 20:10:11 +0200

    Cambios.

---
+++ src/java/persistencia/LlibreDao.java
@@ -0,0 +1,95 @@
+package persistencia;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+import model.*;
+
+public class LlibreDao {
+
+    private Connection con;
+
+    public LlibreDao(Connection con) {
+        this.con = con;
+    }
+
+    public boolean afegir(Llibre llib) {
+        boolean afegit = true;
+        PreparedStatement pt = null;
+        String sentencia = "INSERT INTO LLIBRE(ISBN,TITOL,AUTOR,EDITORIAL,ANYEDICIO,ESTOC)"
+                + " VALUES(?,?,?,?,?,?)";
+        try {
+            pt = con.prepareStatement(sentencia);
+            pt.setString(1, llib.getIsbn());
+            pt.setString(2, llib.getTitol());
+            pt.setString(3, llib.getAutor());
+            pt.setString(4, llib.getEditorial());
+            pt.setInt(5, llib.getAnyEdicio());
+            pt.setInt(6, llib.getEstoc());
+
+            if (pt.executeUpdate() == 0) {
+                afegit = false;
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            afegit = false;
+        } finally {
+            tancarRecurs(pt);
+        }
+
+        return afegit;
+    }
+
+    public Llibre cercarPerISBN(String isbn) {
+        String consulta = " SELECT * FROM LLIBRE WHERE isbn='" + isbn + "'";
+        Statement st;
+        ResultSet rs;
+        Llibre llib = null;
+
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery(consulta);
+            if (rs.next()) {
+                llib = new Llibre(rs.getString(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getInt(5), rs.getInt(6));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return llib;
+    }
+
+    public List<Llibre> cercarTots() {
+        String consulta = " SELECT * FROM LLIBRE";
+        Statement st;
+        ResultSet rs;
+        List<Llibre> llista = new ArrayList<>();
+        try {
+            st = con.createStatement();
+            rs = st.executeQuery(consulta);
+            while (rs.next()) {
+                llista.add(new Llibre(rs.getString(1),
+                        rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6)));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return llista;
+    }
+
+    private void tancarRecurs(AutoCloseable r) {
+        try {
+            r.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        } catch (Exception ex) {
+
+        }
+
+    }
+
+}
